d6ea613d20636d0a00a1207c19174e14cbb724c3
Alex Johnson <alex@tablelist.com>
2015-02-24 19:43:43 -0500

    jQuery for rendering courses onto page

--- assignments/jquery/app.js
+++ assignments/jquery/app.js
@@ -1,2 +1,49 @@
 // JAVASCRIPT LOGIC
 
+$(function () {
+
+  // List of courses in memory
+  var courses = [ 
+    	{ name : "Java 101", category : "PROG", dateCreated : "1/1/2015", description : "Wow" },
+    	{ name : "MongoDB 101", category : "DB", dateCreated : "2/1/2015", description : "Good" },
+    	{ name : "Express 101", category : "PROG", dateCreated : "3/1/2015", description : "Better" }, 
+    	{ name : "AngularJS 101", category : "WEB", dateCreated : "4/1/2015", description : "Best" }, 
+    	{ name : "NodeJS 101", category : "PROG", dateCreated : "5/1/2015", description : "Awesome" }
+  ];
+
+  // Initialize Data onto Page
+  renderCourses(courses);
+
+  // Takes the list of courses, and render them as table rows
+  function renderCourses(courses){
+    
+    // Iterate through courses
+    for (var i = courses.length - 1; i >= 0; i--) {
+      
+      var tr = $("<tr>");
+
+      // Select content to be placed in table
+      var content = [courses[i].name, courses[i].category, courses[i].dateCreated];
+
+      // Create table data cells
+      for (var j = content.length - 1; k=j >= 0; k=j--) {
+        var td = $("<td>");
+        td.append(content[j]);
+        tr.append(td);
+      };
+
+      // HTML for buttons
+      var buttons = '<button type="button" class="btn btn-primary pull-right">';
+      buttons += '<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>';
+      buttons += '</button>';
+
+      // Add EDIT buttons to table row
+      tr.append($("<td>").append(buttons));
+
+      $(".course-list").append(tr);
+    }
+  }
+  
+
+
+});
